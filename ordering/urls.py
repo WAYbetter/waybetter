f5b460424af92b7f039e8be2aa536c7575642519
Udi h Bauman <dibaunaumh@gmail.com>
2010-09-14 16:39:57 +0200

    Moved resolve_address to ordering app, & added initial UI for ride cost estimation

--- ordering/urls.py
+++ ordering/urls.py
@@ -15,10 +15,12 @@ urlpatterns = patterns('',
 
 
     # services
-    (r'^service/book_order/$', 'ordering.passenger_controller.book_order'),
-    (r'^service/update_order_status/$', 'ordering.station_controller.update_order_status'),
-    (r'^service/get_orders/$', 'ordering.station_controller.get_orders'),
-    (r'^service/get_workstation_orders/$', 'ordering.station_controller.get_workstation_orders'),
+    (r'^services/resolve_address/$', 'ordering.passenger_controller.resolve_address'),
+    (r'^services/estimate_cost/$', 'ordering.passenger_controller.estimate_ride_cost'),
+    (r'^services/book_order/$', 'ordering.passenger_controller.book_order'),
+    (r'^services/update_order_status/$', 'ordering.station_controller.update_order_status'),
+    (r'^services/get_orders/$', 'ordering.station_controller.get_orders'),
+    (r'^services/get_workstation_orders/$', 'ordering.station_controller.get_workstation_orders'),
     (r'^services/send_sms_verification/$', 'ordering.passenger_controller.send_sms_verification'),
     (r'^services/register_passenger/$', 'ordering.passenger_controller.register_passenger'),
     (r'^services/login_passenger/$', 'ordering.passenger_controller.login_passenger'),
