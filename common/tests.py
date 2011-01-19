e12990b97bf8694875e2a4012259a69381d8451e
Amir Nissim <amirnissim@gmail.com>
2011-01-19 14:56:15 +0200

    common.tests: geocoding, route and basic functionality

--- common/tests.py
+++ common/tests.py
@@ -1,15 +1,11 @@
 # This Python file uses the following encoding: utf-8
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
+from django.core.urlresolvers import reverse
+from django.test import TestCase
 
-Replace these with more appropriate tests for your application.
-"""
+import common.urllib_adaptor as urllib2
 from common.geo_calculations import distance_between_points
 from common.geocode import geocode, geohash_encode, geohash_decode
-
-from django.test import TestCase
-import common.urllib_adaptor as urllib2
+from common.sms_notification import send_sms
 from common.route import calculate_time_and_distance
 
 import logging
@@ -19,22 +15,26 @@ class GeocodeTest(TestCase):
     Geocoding unitTest: geo coding, geo distance and geo hashing tests.
     """
 
-    def setUp(self):
-        self.test_data = (
+    def test_geo_coding(self):
+        """
+        Check that geocoding returns at least one location with correct geocode,
+        i.e., country, city, street, house number, lon. and lat. are matching those of the query.
+        """
+        test_data = (
+            {"address"      : u"דיזנגוף 99 תל אביב",
+             "country"      : u"IL",
+             "city"         : u"תל אביב יפו",
+             "street"       : u"דיזנגוף",
+             "house_number" : u"99",
+             "lon"          : '34.77388',
+             "lat"          : '32.07933',
+        },
             {"address"      : u"מרג 1 תל אביב יפו",
              "country"      : u"IL",
              "city"         : u"תל אביב יפו",
              "street"       : u"מרגולין",
              "house_number" : u"1",
              "lon"          : '34.787368',
-             "lat"          : '32.05856',
-        },
-            {"address"      : u"מרגולין 15 תל אביב יפו",
-             "country"      : u"IL",
-             "city"         : u"תל אביב יפו",
-             "street"       : u"מרגולין",
-             "house_number" : u"15",
-             "lon"          : '34.790611',
              "lat"          : '32.05856',
         },
             {"address"      : u"בן יהודה 35 ירושלים",
@@ -46,31 +46,29 @@ class GeocodeTest(TestCase):
              "lat"          : '31.780725',
             },
         )
-
-    def test_geo_coding(self):
-        """
-        Check that geocoding returns at least one location with correct geocode,
-        i.e., country, city, street, house number, lon. and lat. are matching those of the query.
-        """
+        
         logging.info("\nTesting geo coding")
-        for test_case in self.test_data:
+        for test_case in test_data:
+            test_case_success = False
+
             address = test_case["address"]
             logging.info("Testing geo coding for %s" % address)
             geo_code = geocode(address)
             self.assertTrue(geo_code, msg="no geo code received for %s" % address)
 
-            # geo_code may contain more than one location. Check that at least one is the correct.
+            # geo_code may contain more than one location. Check that at least one is correct.
             for location in geo_code:
-                success = True
+                location_success = True
                 logging.info("Processing location %s" % location["description"])
 
-                # string properties, compare lowercase
+                # textual properties, compare lowercase
                 for property in ["country", "city", "street", "house_number"]:
                     result = "OK"
                     if not test_case[property].lower() == location[property].lower():
                         result = "failed"
-                        success = False
-#                    logging.info("comparing %s: %s" % (property, result))
+                        location_success = False
+                    #uncomment for debug since all Django tests run with DEBUG=False
+                    #logging.info("comparing %s: %s" % (property, result))
 
                 # numerical properties, allowed to differ a bit.
                 precision = 0.001
@@ -78,14 +76,15 @@ class GeocodeTest(TestCase):
                 for property in ["lon", "lat"]:
                     if not abs(float(test_case[property]) - float(location[property])) < precision:
                         result = "failed"
-                        success = False
-#                    logging.info("comparing %s with precision %f: %s" % (property, precision, result))
+                        location_success = False
+                    #logging.info("comparing %s with precision %f: %s" % (property, precision, result))
 
-                if success:
+                if location_success:
                     logging.info("Found correct location at %s" % location["description"])
+                    test_case_success = True
                     break
 
-            self.assertTrue(success, msg="correct geo code was not found")
+            self.assertTrue(test_case_success, msg="correct geo code was not found for %s" % address)
 
 
     def test_geo_distance(self):
@@ -93,8 +92,10 @@ class GeocodeTest(TestCase):
         Test geo distance calculation. Taken from Jan Matuschek's article.
         """
         logging.info("\nTesting geo distance liberty to eiffel")
-        # distance is about 5837 km, ignore decimal places
-        self.assertAlmostEqual(distance_between_points(40.6892, -74.0444, 48.8583, 2.2945), 5837.0, places=0, msg="geo distanec error")
+        # distance is about 5837 km, check 3 decimal places
+        d = distance_between_points(40.6892, -74.0444, 48.8583, 2.2945)
+        expected_d = 5837.413
+        self.assertAlmostEqual(d, expected_d, places=3, msg="geo distance error %f (expected %f)" % (d, expected_d))
 
     def test_geo_hash(self):
         """
@@ -111,31 +112,6 @@ class GeocodeTest(TestCase):
         for p in points:
             self.assertEqual(geohash_encode(float(p["lon"]),float(p["lat"])), p["hash_code"], msg="encode error")
             self.assertEqual(geohash_decode(p["hash_code"]), (p["lon"],p["lat"]), msg="decode error")
-
-
-
-class Urllib2AdaptorTest(TestCase):
-    def test_fetch_url(self):
-        content = urllib2.urlopen("http://www.google.com")
-        self.assertTrue("google" in str(content))
-
-    def test_content_read(self):
-        response = urllib2.urlopen("http://www.google.com")
-        content = response.read()
-        self.assertTrue("google" in content)
-
-    def test_request_headers(self):
-        mobile_str = "mobile.twitter.com"
-        url = "http://www.twitter.com"
-        request = urllib2.Request(url,
-                                  headers={"USER_AGENT": "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/420.1 (KHTML, like Gecko) Version/3.0 Mobile/1A542a Safari/419.3"})
-
-        content = urllib2.urlopen(request)
-        self.assertTrue(mobile_str in str(content))
-
-        request = urllib2.Request(url)
-        content = urllib2.urlopen(request)
-        self.assertTrue(mobile_str not in str(content))
 
 
 class RouteTest(TestCase):
@@ -181,3 +157,53 @@ class RouteTest(TestCase):
 
             logging.info("Distance received: %d (expected %d)" % (d, expected_d))
             self.assertTrue(1 - self.ERR_RATIO < d/expected_d < 1 + self.ERR_RATIO)
+
+
+class Urllib2AdaptorTest(TestCase):
+    def test_fetch_url(self):
+        content = urllib2.urlopen("http://www.google.com")
+        self.assertTrue("google" in str(content))
+
+    def test_content_read(self):
+        response = urllib2.urlopen("http://www.google.com")
+        content = response.read()
+        self.assertTrue("google" in content)
+
+    def test_request_headers(self):
+        mobile_str = "mobile.twitter.com"
+        url = "http://www.twitter.com"
+        request = urllib2.Request(url,
+                                  headers={"USER_AGENT": "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/420.1 (KHTML, like Gecko) Version/3.0 Mobile/1A542a Safari/419.3"})
+
+        content = urllib2.urlopen(request)
+        self.assertTrue(mobile_str in str(content))
+
+        request = urllib2.Request(url)
+        content = urllib2.urlopen(request)
+        self.assertTrue(mobile_str not in str(content))
+
+
+class BasicFuncTest(TestCase):
+    """
+    Test basic functionality provided by common app.
+    """
+
+    fixtures = ['countries']
+    
+    def test_send_sms(self):
+        logging.info("\nTesting sms sending")
+        self.assertTrue(send_sms('+972-3-1234567', 'sms test'), msg="sms send failed")
+
+    def test_is_username_available(self):
+        logging.info("\nTesting username available")
+        from django.contrib.auth.models import User
+        test_user = User()
+        test_user.username = "test_user1"
+        test_user.save()
+
+        response = self.client.get(reverse('common.services.is_username_available'), {"username": "test_user1"})
+        self.assertTrue(response.content == "false", msg="expected username unavailable")
+
+        response = self.client.get(reverse('common.services.is_username_available'), {"username": "test_user2"})
+        self.assertTrue(response.content == "true", msg="expected username unavailable")
+
