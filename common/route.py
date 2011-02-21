14fefb95f95b75d0fd9c4df046acf9d445ff90d7
Amir Nissim <amirnissim@gmail.com>
2011-02-21 13:48:22 +0200

    use telmap for route calculation and refactor (x,y) to (lon,lat)

--- common/route.py
+++ common/route.py
@@ -1,22 +1,40 @@
+import logging
 import common.urllib_adaptor as urllib2
 from django.utils import simplejson
 from django.conf import settings
 from datetime import datetime
+from geocode import telmap_request, get_text_from_element
 
+def calculate_time_and_distance(from_lon, from_lat, to_lon, to_lat):
+    return telmap_calculate_time_and_distance(from_lon, from_lat, to_lon, to_lat)
 
-def calculate_time_and_distance(from_x, from_y, to_x, to_y):
-    return waze_calculate_time_and_distance(from_x, from_y, to_x, to_y)
+def telmap_calculate_time_and_distance(from_lon, from_lat, to_lon, to_lat, return_cities=False, return_streets=False):
+    """
+    Uses the Telmap API (http://api.navigator.telmap.com/xmlwiki/Wiki.jsp?page=General#section-General-RouteRequest)
+    to calculate the time &amp; distance between the 2 given locations.
+    """
+    logging.info("telmap_route")
+    route_response = telmap_request({"from_lon": from_lon, "from_lat": from_lat, "to_lon": to_lon, "to_lat": to_lat},
+                                   "telmap_route.xml")
+    d = float(get_text_from_element(route_response, "totalDistanceInMeters"))
+    t = float(get_text_from_element( route_response, "totalTimeSeconds"))
 
+    result = {
+        "estimated_distance":   d,
+        "estimated_duration":   t
+    }
 
-def waze_calculate_time_and_distance(from_x, from_y, to_x, to_y, return_cities=False, return_streets=False):
+    return result
+
+def waze_calculate_time_and_distance(from_lon, from_lat, to_lon, to_lat, return_cities=False, return_streets=False):
     """
     Uses the Waze API (http://www.waze.co.il/RoutingManager/routingRequest)
     to calculate the time &amp; distance between the 2 given locations.
     Note that only the 1st alternative path Waze provides is considered.
     """
     url = 'http://www.waze.co.il/RoutingManager/routingRequest?' + \
-            'from=x:%s+y:%s+bd:true' % (from_x, from_y) + \
-            '&to=x:%s+y:%s+bd:true+st_id:46317' % (to_x, to_y) + \
+            'from=x:%s+y:%s+bd:true' % (from_lon, from_lat) + \
+            '&to=x:%s+y:%s+bd:true+st_id:46317' % (to_lon, to_lat) + \
             '&returnJSON=true&returnGeometries=true&returnInstructions=false' + \
             '&timeout=60000&nPaths=1&token=%s' % settings.WAZE_API_TOKEN
     json = urllib2.urlopen(url).read()
