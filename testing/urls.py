5c44f0df46086ae08a96a1ff5f5f52e08586b7c0
Amir Nissim <amirnissim@gmail.com>
2011-02-24 16:11:19 +0200

    selenium tests refactoring

--- testing/urls.py
+++ testing/urls.py
@@ -3,8 +3,8 @@ from django.views.generic.simple import direct_to_template
 
 
 urlpatterns = patterns('',
-    (r'create_selenium_test_data/$', 'testing.test_data.create_selenium_test_data'),
-    (r'destroy_selenium_test_data/$', 'testing.test_data.destroy_selenium_test_data'),
+    (r'create_selenium_test_data/$', 'testing.setup_testing_env.create_selenium_test_data'),
+    (r'destroy_selenium_test_data/$', 'testing.setup_testing_env.destroy_selenium_test_data'),
 )
 
  
