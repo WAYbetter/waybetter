67fc3fb8714ab669fc35b2e3036fbdf7f4dc85fe
Guy Kremnitzer <guykrem@gmail.com>
2011-07-17 15:21:05 +0300

    support accept-language http header from iphone native

--- common/middleware/overrideBrowser.py
+++ common/middleware/overrideBrowser.py
@@ -5,7 +5,8 @@ class LanguageMiddleware:
     This middleware overrides browser langauge defaults.
     """
     def process_request(self, request):
-        if not request.session.get("browser_lang_overriden", None):
+        # respect accept lang for iphone native app
+        if request.META.get("HTTP_USER_AGENT", "").find("Darwin") < 0 and not request.session.get("browser_lang_overriden", None):
             request.session["browser_lang_overriden"] = True
             request.session['django_language'] = settings.LANGUAGE_CODE
 
