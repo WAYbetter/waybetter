ddfdebe35c5d0683f210b2c725b710c3a6bd8e1a
Guy Kremnitzer <guykrem@gmail.com>
2011-09-18 15:34:30 +0300

    fix login url issue

--- common/registration.py
+++ common/registration.py
@@ -8,7 +8,7 @@ from ordering.models import Passenger, Feedback
 
 FACEBOOK_APP_ID = getattr(settings, 'FACEBOOK_APP_ID', '')
 def get_login_form(request):
-    return render_to_response('login_form.html', {'FACEBOOK_APP_ID': FACEBOOK_APP_ID})
+    return render_to_response('login_form.html', {'FACEBOOK_APP_ID': FACEBOOK_APP_ID, 'next': '/social/login/done/'})
 
 def feedback(request):
     passenger = Passenger.from_request(request)
