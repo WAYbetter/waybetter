75b3ac9aab4fbac1e809b8f141fc75d76a42d012
Amir Nissim <amirnissim@gmail.com>
2011-10-03 11:05:51 +0200

    chrome frame, random banner color

--- static/js/utils.js
+++ static/js/utils.js
@@ -460,3 +460,8 @@ Array.prototype.sum = function(){
     for (var i = 0,sum = 0; i < this.length; sum += this[i++]);
     return sum;
 };
+
+function getRandomInt(min, max)
+{
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}  
