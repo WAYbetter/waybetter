a5c42e0ad7b31b848965775d533afcb8e33d456a
Amir Nissim <amirnissim@gmail.com>
2012-01-16 14:00:00 +0200

    more station business page

--- common/middleware/station_domain_middleware.py
+++ common/middleware/station_domain_middleware.py
@@ -1,22 +1,14 @@
-from ordering.station_controller import station_page
+from ordering.station_controller import station_page, get_station_domain
 
-STATION_DOMAINS = ["taxiapp.co.il"]
+
 class StationDomainMiddleware(object):
-    @staticmethod
-    def get_station_domain(request):
-        if request.path == "/": # only affect requests to root
-            host = request.META.get("SERVER_NAME", None)
-            for domain in STATION_DOMAINS:
-                if host.endswith(domain):
-                    return host.replace('www.','').split(".")[0].lower()
-
-        return None
-
     def process_request(self, request):
-        subdomain_name = self.get_station_domain(request) 
+        if request.path == "/": # only affect requests to root
+            subdomain_name = get_station_domain(request)
 
-        if not subdomain_name or subdomain_name == 'taxiapp':
-            return None # no redirect - continue normal processing
-        else:
-            return station_page(request, subdomain_name) # serve station page instead of normal home page
+            if subdomain_name and subdomain_name != 'taxiapp':
+                return station_page(request, subdomain_name) # serve station page instead of normal home page
+
+        return None # no redirect - continue normal processing
+
 
