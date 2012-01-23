cc6791f373b8822da080bdd5314c346e9c9277de
Amir Nissim <amirnissim@gmail.com>
2012-01-23 18:50:55 +0200

    ISR testing page

--- fleet/urls.py
+++ fleet/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^auth/$', 'fleet.views.authenticate'),
+    (r'^isr/$', 'fleet.views.isr_view'),
 )
--- fleet/views.py
+++ fleet/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+from djangotoolbox.http import JSONResponse
 from suds.client import Client
+from backends import isr as isr_backend
+import inspect
 import logging
 
 def authenticate(request):
@@ -13,3 +18,30 @@ def authenticate(request):
     logging.info(auth_number)
     return HttpResponse(auth_number)
 
+
+def isr_view(request):
+    if request.method == "POST":
+        result = ""
+        method_name = request.POST.get("method_name")
+        try:
+            method = getattr(isr_backend, method_name)
+            args = inspect.getargspec(method)[0]
+            values = [request.POST.get(arg) for arg in args]
+            result = method(*values)
+
+        except Exception, e:
+            result = e.message
+
+        return JSONResponse({'result': result})
+    else:
+        methods = []
+        method_type = type(lambda x: x)
+        for attr_name in dir(isr_backend):
+            if attr_name.startswith("_"):
+                continue
+            attr = getattr(isr_backend, attr_name)
+            if type(attr) == method_type:
+                args = inspect.getargspec(attr)[0]
+                methods.append({'name': attr.func_name, 'args': args, 'doc': attr.func_doc or ""})
+
+        return render_to_response("isr_view.html", locals(), RequestContext(request))
