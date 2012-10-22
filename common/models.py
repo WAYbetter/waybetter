230eb77c811e79468adbba63c5fea772888cb038
Guy Kremnitzer <guykrem@gmail.com>
2012-10-22 17:54:20 +0200

    add dirty fields checking to BaseModel

--- common/models.py
+++ common/models.py
@@ -37,6 +37,18 @@ class BaseModel(models.Model):
     class Meta:
         abstract = True
 
+    def __init__(self, *args, **kwargs):
+        super(BaseModel, self).__init__(*args, **kwargs)
+        self._original_state = self._as_dict()
+
+    def _as_dict(self):
+        return dict([(f.name, getattr(self, f.name)) for f in self._meta.local_fields if not f.rel])
+
+    @property
+    def dirty_fields(self):
+        new_state = self._as_dict()
+        return dict([(key, value) for key, value in self._original_state.iteritems() if value != new_state[key]])
+
     def fresh_copy(self):
         return type(self).by_id(self.id)
 
