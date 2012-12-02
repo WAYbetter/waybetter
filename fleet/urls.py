fda21ff4ac7b06d7421d078c7b1aa5e3aeaf611c
Amir Nissim <amirnissim@gmail.com>
2012-12-02 09:09:17 +0200

    remove get_ongoing_rides for isrproxy

--- fleet/urls.py
+++ fleet/urls.py
@@ -7,12 +7,8 @@ urlpatterns = patterns('',
     (r'^isr/status$', 'fleet.views.isr_status_page'),
     (r'^isr/services/get_rides/$', 'fleet.views.get_ride_events'),
 
-    (r'^isrproxy/get_ongoing_rides/$', 'fleet.backends.isr_proxy.get_ongoing_rides'),
     (r'^isrproxy/update/status/$', 'fleet.backends.isr_proxy.update_status'),
     (r'^isrproxy/update/positions/$', 'fleet.backends.isr_proxy.update_positions'),
-
-
-
 
     # TODO_WB: remove url and controller when ISR testing is done
     (r'^isrproxy/create/nyride/(?P<ride_id>\d+)/$', 'fleet.views.create_ny_isr_ride'),
