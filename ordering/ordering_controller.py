bc7b6aac94d49d4dc2a72f55b5f0191fe8c7aed6
Guy Kremnitzer <guykrem@gmail.com>
2012-12-05 20:29:56 +0200

    handle new rides as well

--- ordering/ordering_controller.py
+++ ordering/ordering_controller.py
@@ -602,11 +602,12 @@ def book_ride(request):
 def join_offer_and_order(order, request_data):
     # connect the order the the offer that was clicked
     last_search_req = SearchRequest.objects.filter(passenger=order.passenger).order_by("-create_date")[0]
-    logging.info("joining order %s to offers from search request %s" % (order, last_search_req))
+    logging.info("joining order %s to offers from search request %s" % (order.id, last_search_req.id))
     offer_ride_key = request_data.get("ride_id")
     if last_search_req:
         for offer in last_search_req.offers.all():
-            if offer.ride_key == str(offer_ride_key):
+            if offer.ride_key == (str(offer_ride_key) if offer_ride_key else None):
+                logging.info("offer %s was joined with order %s" % (offer.id, order.id))
                 offer.order = order
                 offer.save()
                 break
