f16470777bfa08d8e0bad8289d1006930c22ead8
Amir Nissim <amirnissim@gmail.com>
2013-01-17 12:42:55 +0200

    fixes #3

--- common/middleware/overrideBrowser.py
+++ common/middleware/overrideBrowser.py
@@ -1,23 +1,27 @@
 import settings
+import logging
+
+DJANGO_LANGUAGE = 'django_language'
+BROWSER_LANG_OVERRIDEN = 'browser_lang_overriden'
 
 class LanguageMiddleware:
     """
     This middleware overrides browser langauge defaults.
     """
     def process_request(self, request):
-        lang_to_set = None
+        if not request.session.get(BROWSER_LANG_OVERRIDEN):
+            lang_to_set = None
 
-        # respect 1.2.1 mobile app language preference
-        if request.META.get("HTTP_LANGUAGE"):
-            lang_to_set = request.META.get("HTTP_LANGUAGE")
+            # respect 1.2.1 mobile app language preference
+            if request.META.get("HTTP_LANGUAGE"):
+                lang_to_set = request.META.get("HTTP_LANGUAGE")
 
-        # respect accept lang for iphone native app
-        elif request.META.get("HTTP_USER_AGENT", "").find("Darwin") < 0 and not request.session.get("browser_lang_overriden", None):
-            request.session["browser_lang_overriden"] = True
-            lang_to_set =  settings.LANGUAGE_CODE
+            # respect accept lang for iphone native app
+            elif request.META.get("HTTP_USER_AGENT", "").find("Darwin") < 0:
+                lang_to_set =  settings.LANGUAGE_CODE
 
-
-        if lang_to_set:
-            request.session['django_language'] = lang_to_set
-
+            if lang_to_set:
+                logging.info("[process_request] settings language to %s" % lang_to_set)
+                request.session[DJANGO_LANGUAGE] = lang_to_set
+                request.session[BROWSER_LANG_OVERRIDEN] = True
   
